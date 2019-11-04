78df70828f62c2450408309e7afd1a91bfcc0d52
Mauricio Ribeiro <mauricio.ribeiro@climatempo.com.br>
2019-11-03 21:06:21 -0300

    Implementa envio unitario

--- static/js/home.controller.js
+++ static/js/home.controller.js
@@ -6,23 +6,20 @@ angular.module('whatsapp').controller('HomeController', (API_URL, $http, $scope)
     };
 
     $scope.list = () => {
-        $http({
-            url: API_URL + '/instances'
-        }).then((response) => {
-            $scope.instances = response.data.instances || [];
-        }).catch(() => {
-            $scope.error = true;
-        });
+        $http.get(API_URL + '/instances')
+            .then((response) => {
+                $scope.instances = response.data.instances || [];
+            }).catch(() => {
+                $scope.error = true;
+            });
     };
 
     $scope.start = () => {
-        $http({
-            url: API_URL + '/start/' + $scope.input.instance,
-            method: 'POST'
-        }).then((response) => {
-            $scope.input.instance = null;
-            $scope.list();
-        })
+        $http.post(API_URL + '/start/' + $scope.input.instance)
+            .then((response) => {
+                $scope.input.instance = null;
+                $scope.list();
+            });
     };
 
     $scope.list();
--- static/js/instance.controller.js
+++ static/js/instance.controller.js
@@ -11,19 +11,24 @@ angular.module('whatsapp').controller('InstanceController', (API_URL, $http, $sc
     $scope.get = () => {
         let params = $location.absUrl().split('/');
 
-        $http({
-            url: API_URL + '/status/' + params[params.length - 1]
-        }).then((response) => {
+        $http.get(API_URL + '/status/' + params[params.length - 1])
+            .then((response) => {
             $scope.instance = response.data;
-        })
+        });
     };
 
     $scope.canSend = () => {
-        return $scope.input.msg.to && $scope.input.msg.message && $scope.instance === 'ONLINE';
+        return $scope.input.msg.to && $scope.input.msg.message && $scope.instance.status === 'CONNECTED';
     };
 
     $scope.send = () => {
+        let msg = $scope.input.msg;
+        msg.to = '55' + msg.to;
 
+        $http.post(API_URL + '/send/' + $scope.instance.instance, msg)
+            .then((response) => {
+                alert('Mensagem enviada!');
+            });
     };
 
     $scope.get();
