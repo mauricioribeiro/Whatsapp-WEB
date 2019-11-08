7b93c261a9824265847d9b53138bb8d6a9a46d84
Mauricio Ribeiro <mauricio.ribeiro@climatempo.com.br>
2019-11-07 22:48:38 -0300

    Adiciona tabela de recebimento de mensagens

--- static/js/instance.controller.js
+++ static/js/instance.controller.js
@@ -1,5 +1,6 @@
 angular.module('whatsapp').controller('InstanceController', (API_URL, $http, $scope, $location) => {
     $scope.instance = {};
+    $scope.messages = [];
     $scope.input = {
         msg: {
             to: null,
@@ -20,12 +21,15 @@ angular.module('whatsapp').controller('InstanceController', (API_URL, $http, $sc
                 socket.emit('subscribe', {channel: '/instances/' + $scope.instance.instance });
 
                 socket.on('checkin', (instance) => {
+                    console.log(instance);
                     $scope.instance = instance;
                     $scope.$apply();
                 });
 
                 socket.on('receive', (message) => {
                     console.log(message);
+                    $scope.messages.unshift(message);
+                    $scope.$apply();
                 });
         });
     };
@@ -42,6 +46,21 @@ angular.module('whatsapp').controller('InstanceController', (API_URL, $http, $sc
             .then((response) => {
                 alert('Mensagem enviada!');
             });
+    };
+
+    $scope.stop = () => {
+        $http.post(API_URL + '/stop/' + $scope.instance.instance)
+            .then((response) => {
+                alert('Instancia reiniciada!');
+            });
+    };
+
+    $scope.isImage = (message) => {
+        return message && message.match(/^\/[^"]*/gm) != null;
+    };
+
+    $scope.getImage = (image) => {
+        return 'data:image/png;base64,' + image;
     };
 
     $scope.get();
